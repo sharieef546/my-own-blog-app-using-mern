239f1893e2d83c8eb447b2cef6562783498ebc45
jaffer <jaffershareefshaik@gmail.com>
2024-09-01 10:34:59 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app=express();
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
-});
+
+
+const app = express();
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://user2:jaffer@me.nelux.mongodb.net/?retryWrites=true&w=majority&appName=me')
+    .then(() => {
+        console.log("MongoDB is connected");
+
+        // Start the Express server
+        app.listen(3000, () => {
+            console.log("Server is running on port 3000");
+        });
+    })
+    .catch((error) => {
+        console.error("Error connecting to MongoDB:", error);
+    });
